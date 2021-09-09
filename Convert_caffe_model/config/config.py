078a779289406ab0011ce96f214d7752a3f771e2
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2021-09-09 11:32:05 +0700

    updating file config convert

--- Convert_caffe_model/config/config.py
+++ Convert_caffe_model/config/config.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MODEL_DIR = BASE_DIR / 'weights' / 'models' / 'VGGNet' / 'VOC0712' / 'SSD_300x300_ft'
+MODEL_DIR = BASE_DIR / 'weights' / 'models' / 'VGGNet' / 'VOC0712' / 'SSD_300x300'
 MODEL_PROTO = MODEL_DIR / 'deploy.prototxt'
-MODEL_WEIGHTS = MODEL_DIR / 'VGG_VOC0712_SSD_300x300_ft_iter_120000.caffemodel'
+MODEL_WEIGHTS = MODEL_DIR / 'VGG_VOC0712_SSD_300x300_iter_120000.caffemodel'
 
 OUTPUT_WEIGHTS = BASE_DIR / 'weights' / 'models' / 'ssd300_voc_weights.hdf5'
 OUTPUT_SHAPE = BASE_DIR / 'weights' / 'models' / 'ssd300_voc_shape.pkl'
